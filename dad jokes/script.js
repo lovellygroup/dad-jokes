da2f4ccb6e820cfc22a95f2cc5dc95c820e45ae6
lovellygroup <161957704+lovellygroup@users.noreply.github.com>
2025-08-09 21:28:38 +0200

    Update script.js

--- dad jokes/script.js
+++ dad jokes/script.js
@@ -5,7 +5,7 @@ const jokeBtn = document.getElementById("jokeBtn");
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="styl090909090esheet" href="css/style.css">
   <link rel="stylesheet" href="css/all.min.css">
   <title>يوم التأسيس</title>
   <link rel="icon" href="imgs/icon.png">
